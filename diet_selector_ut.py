2b790d022b1a1e5f0bed611b1aae0b3a7e2d0e39
jajcek <jajcek.30@wp.pl>
2013-04-27 21:34:04 +0200

    test for calculating decision values

--- diet_selector_ut.py
+++ diet_selector_ut.py
@@ -16,8 +16,12 @@ class TestDietSelector( unittest.TestCase ):
 		self.m2 = [[1.0, 1.0/5.0, 1.0],
 				   [5.0,     1.0, 3.0],
 				   [1.0, 1.0/3.0, 1.0]]
+				   
+		self.m3 = [[    1.0,     1.0, 7.0],
+				   [    1.0,     1.0, 3.0],
+				   [1.0/7.0, 1.0/3.0, 1.0]]
 			  
-		self.m3 = [[    1.0, 7.0, 9.0],
+		self.m4 = [[    1.0, 7.0, 9.0],
 				   [1.0/7.0, 1.0, 1.0],
 				   [1.0/9.0, 1.0, 1.0]]
 				   
@@ -39,11 +43,25 @@ class TestDietSelector( unittest.TestCase ):
 		diet_selector.normalizeVertically( self.m1 )
 		diet_selector.normalizeVertically( self.m2 )
 		diet_selector.normalizeVertically( self.m3 )
-		s = diet_selector.calcMatrixWithAvgRows( self.m1, self.m2, self.m3 );
+		diet_selector.normalizeVertically( self.m4 )
+		s = diet_selector.calcMatrixWithAvgRows( self.m1, self.m2, self.m3, self.m4 );
 		self.assertEqual( s,
 			[[0.5105339105339105, 0.3893217893217893, 0.10014430014430013],
 			 [0.1577639751552795, 0.6554865424430641, 0.1867494824016563],
+			 [0.5105339105339105, 0.3893217893217893, 0.10014430014430013],
 			 [0.7978093167966587, 0.10531475088437114, 0.09687593231897029]] )
+
+	def test_calcDecisionValues( self ):
+		diet_selector.normalizeVertically( self.m0 )
+		diet_selector.normalizeVertically( self.m1 )
+		diet_selector.normalizeVertically( self.m2 )
+		diet_selector.normalizeVertically( self.m3 )
+		diet_selector.normalizeVertically( self.m4 )
+		s0 = diet_selector.calcMatrixWithAvgRows( self.m0 );
+		s  = diet_selector.calcMatrixWithAvgRows( self.m1, self.m2, self.m3, self.m4 );
+		r = diet_selector.calcDecisionValues( s0, s )
+		self.assertEqual( r,
+			[0.43112114650883737, 0.4461897715261283, 0.12268908196503436] )
 
 			
 if __name__ == '__main__':			
