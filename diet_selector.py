ceb8e601435ab69b62b80b36e9058775f447cf2b
mwendel <michal.wendel@gmail.com>
2013-04-28 17:14:52 +0200

    consistency added

--- diet_selector.py
+++ diet_selector.py
@@ -21,8 +21,13 @@ m4 = [[    1.0, 7.0, 9.0],
 	  [1.0/7.0, 1.0, 1.0],
 	  [1.0/9.0, 1.0, 1.0]]
 
+
+RI = [    0,    0, 0.58, 0.90, 1.12,
+	   1.24, 1.32, 1.41, 1.45, 1.49 ];
+
+
 def main():
-	normalizeVertically( m0 )
+	"""normalizeVertically( m0 )
 	normalizeVertically( m1 )
 	normalizeVertically( m2 )
 	normalizeVertically( m3 )
@@ -30,8 +35,17 @@ def main():
 	s0 = calcMatrixWithAvgRows( m0 )
 	s  = calcMatrixWithAvgRows( m1, m2, m3, m4 )
 	r  = calcDecisionValues( s0, s )
-	u  = prepareDecisionVector( r )
+	u  = prepareDecisionVector( r )"""
+	u = lambdaMax( m0 );
+	w = consistencyIndex( u, len( m0[0] ) );
+	c = consistencyRatio( w, RI[len( m0[0] )-1] )
+	i = isMatrixConsistence( m0 )
+	ii = countMatrixConsistence( m0 )
 	print u
+	print w
+	print c
+	print ii
+	print i
 	
 def normalizeVertically( matrix ):
 	"""Normalizes the given matrix vertically"""
@@ -82,6 +96,43 @@ def prepareDecisionVector( valVec ):
 		index += 1
 	
 	return u
+
+
+
+def gMean( matrixRow ):
+	product = 1.0;
+	for i in matrixRow :
+		product *= i; 
+	return ( product ** ( 1.0 / len( matrixRow ) ) );
+
+def consistencyIndex( lMax, matrixSize ): 
+	return ( ( lMax - matrixSize ) / ( matrixSize - 1 ) );
+
+def consistencyRatio( ci, ri ):
+	return ( ci / ri );
+ 
+def lambdaMax( matrix, ri = RI ):
+	"""Check if matrix is consistence"""
+	"""epsilon [0;1] - error margin"""
+	gMeans = [ gMean( mat ) for mat in matrix ]; 
+	gMeanSum = sum( gMeans );  
+	
+	priorityVector = [ ( gm / gMeanSum ) for gm in gMeans ];
+
+	colSums = [ sum(x) for x in zip(*matrix) ];  
+
+	priorityRow = [ ( colSums[x] * priorityVector[x] ) for x in range( len( colSums ) ) ];
+
+	return sum( priorityRow );
+
+def countMatrixConsistence( matrix, ri = RI ): 
+	c = consistencyIndex( lambdaMax( matrix, ri ), len( matrix[0] ) );
+	r = ri[len( matrix[0] )-1];
+	return consistencyRatio( c, r );
+
+def isMatrixConsistence( matrix, ri = RI, epsilon = 0.1 ): 
+	return ( countMatrixConsistence( matrix, ri ) < epsilon );
+
 
 if __name__ == '__main__':
 	main()
