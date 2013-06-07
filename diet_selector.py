3390099eb5413d75460cef47021f200138c8e501
mwendel <michal.wendel@gmail.com>
2013-06-07 19:31:31 +0200

    new matrices

--- diet_selector.py
+++ diet_selector.py
@@ -22,104 +22,67 @@ _19 = 1.0/9.0
 	   
 CRITERIA = ( 'PRICE', 'NOUR', 'TIME', 'DIGESTIBILITY', 'CALORIFIC', 'SIMPLICITY' )
 COURSES = ( 'GYROS', 'SUSHI', 'FRUIT SALAD', 'PIZZA', 'SCRAMBLED EGGS', 'TOSTS', 'TOMATO SOUP', 'CHICKEN SOUP', 'ROASTED SALMON', 'VEGETABLE SALAD' )
-       
-criteria = \
-[ 
-	[ 1.0, _15, _14 ],
-	[ 5.0, 1.0, 3.0 ],
-	[ 4.0, _13, 1.0 ],
-]
- 
+
 price = \
 [
-	[ 1.0, 6.0, _13, 2.0, _17, _16, _17, _17, 2.0, _18 ],	# gyros 
-	[ _16, 1.0, _18, _14, _19, _19, _18, _19, _12, _19 ],	# sushi
-	[ 3.0, 8.0, 1.0, 7.0, _15, _14, _13, _12, 4.0, _12 ],	# salatka owocowa
-	[ _12, 4.0, _17, 1.0, _18, _17, _16, _17, _12, _18 ],	# pizza
-	[ 7.0, 9.0, 5.0, 8.0, 1.0, _12, 4.0, 3.0, 8.0, 2.0 ],	# jajecznica
-	[ 6.0, 9.0, 4.0, 7.0, 2.0, 1.0, 3.0, 2.0, 9.0, 2.0 ],	# tosty
-	[ 7.0, 8.0, 3.0, 6.0, _14, _13, 1.0, 2.0, 7.0, 5.0 ],	# zupa pomidorowa
-	[ 7.0, 9.0, 2.0, 7.0, _13, _12, _12, 1.0, 8.0, 2.0 ],	# rosol
-	[ _12, 2.0, _14, 2.0, _18, _19, _17, _18, 1.0, _18 ],	# losos z piekarnika
-	[ 8.0, 9.0, 2.0, 8.0, _12, _12, _15, _12, 8.0, 1.0 ]	# salatka warzywna
+	[ 1.0, 3.0, _15, _17, _13 ],	# gyros 4
+	[ _13, 1.0, _17, _19, _15 ],	# sushi 5
+	[ 5.0, 7.0, 1.0, _13, 3.0 ],	# salatka owocowa 2
+	[ 7.0, 9.0, 3.0, 1.0, 5.0 ],	# zupa pomidorowa 1
+	[ 3.0, 5.0, _13, _15, 1.0 ],	# losos z piekarnika 3
 ]
 
 nour = \
 [
-	[ 1.0, _16, 5.0, 4.0, _14, _12, 2.0, 3.0, _15, _13 ],	# gyros 
-	[ 6.0, 1.0, 9.0, 9.0, 3.0, 5.0, 7.0, 8.0, 2.0, 4.0 ],	# sushi
-	[ _15, _19, 1.0, _12, _18, _16, _14, _13, _19, _17 ],	# salatka owocowa
-	[ _14, _19, 2.0, 1.0, _17, _15, _13, _12, _18, _16 ],	# pizza
-	[ 4.0, _13, 8.0, 7.0, 1.0, 3.0, 5.0, 6.0, _12, 2.0 ],	# jajecznica
-	[ 2.0, _15, 6.0, 5.0, _13, 1.0, 3.0, 4.0, _14, _12 ],	# tosty
-	[ _12, _17, 4.0, 3.0, _15, _13, 1.0, 2.0, _16, _14 ],	# zupa pomidorowa
-	[ _13, _18, 3.0, 2.0, _16, _14, _12, 1.0, _17, _15 ],	# rosol
-	[ 5.0, _12, 9.0, 8.0, 2.0, 4.0, 6.0, 7.0, 1.0, 3.0 ],	# losos z piekarnika
-	[ 3.0, _14, 7.0, 6.0, _12, 2.0, 4.0, 5.0, _13, 1.0 ]	# salatka warzywna
+	[ 1.0, _19, _15, _13, _17 ],	# gyros 5
+	[ 9.0, 1.0, 5.0, 7.0, 3.0 ],	# sushi 1
+	[ 5.0, _15, 1.0, 3.0, _13 ],	# salatka owocowa 3
+	[ 3.0, _17, _13, 1.0, _15 ],	# zupa pomidorowa 4
+	[ 7.0, _13, 3.0, 5.0, 1.0 ],	# losos z piekarnika 2
 ]
  
 time = \
-[
-	[ 1.0, 3.0, _17, 2.0, _15, _18, _12, _13, _14, _16 ],	# gyros 
-	[ _13, 1.0, _19, _12, _17, _19, _14, _15, _16, _18 ],	# sushi
-	[ 7.0, 9.0, 1.0, 8.0, 3.0, _12, 6.0, 5.0, 4.0, 2.0 ],	# salatka owocowa
-	[ _12, 2.0, _18, 1.0, _16, _19, _13, _14, _15, _17 ],	# pizza
-	[ 5.0, 7.0, _13, 6.0, 1.0, _14, 4.0, 3.0, 2.0, _12 ],	# jajecznica
-	[ 8.0, 9.0, 2.0, 9.0, 4.0, 1.0, 7.0, 6.0, 5.0, 3.0 ],	# tosty
-	[ 2.0, 4.0, _16, 3.0, _14, _17, 1.0, _12, _13, _15 ],	# zupa pomidorowa
-	[ 3.0, 5.0, _15, 4.0, _13, _16, 2.0, 1.0, _12, _14 ],	# rosol
-	[ 4.0, 6.0, _14, 5.0, _12, _15, 3.0, 2.0, 1.0, _13 ],	# losos z piekarnika
-	[ 6.0, 8.0, _12, 7.0, 2.0, _13, 5.0, 4.0, 3.0, 1.0 ]	# salatka warzywna
+[ 
+	[ 1.0, 3.0, _15, _13, _17 ],	# gyros 4
+	[ _13, 1.0, _17, _15, _19 ],	# sushi 5
+	[ 5.0, 7.0, 1.0, 3.0, _13 ],	# salatka owocowa 2
+	[ 3.0, 5.0, _13, 1.0, _15 ],	# zupa pomidorowa 3
+	[ 7.0, 9.0, 3.0, 5.0, 1.0 ],	# losos z piekarnika 1
 ]
 
 	
 digestibility = \
-[
-	[ 1.0, _14, _18, 2.0, _16, 2.0, _13, _12, _15, _17 ],	# gyros 
-	[ 4.0, 1.0, _15, 5.0, _13, 5.0, 2.0, 3.0, _12, _14 ],	# sushi
-	[ 8.0, 5.0, 1.0, 9.0, 3.0, 9.0, 6.0, 7.0, 4.0, 2.0 ],	# salatka owocowa
-	[ _13, _16, _19, _12, _18, _12, _15, _14, _17, _19 ],	# pizza
-	[ 6.0, 3.0, _13, 7.0, 1.0, 7.0, 4.0, 5.0, 2.0, _12 ],	# jajecznica
-	[ _12, _15, _19, 1.0, _17, 1.0, _14, _13, _16, _18 ],	# tosty
-	[ 3.0, _12, _16, 4.0, _14, 4.0, 1.0, 2.0, _13, _15 ],	# zupa pomidorowa
-	[ 2.0, _13, _17, 3.0, _15, 3.0, _12, 1.0, _14, _16 ],	# rosol
-	[ 5.0, 2.0, _14, 6.0, _12, 6.0, 3.0, 4.0, 1.0, _13 ],	# losos z piekarnika
-	[ 7.0, 4.0, _12, 8.0, 2.0, 8.0, 5.0, 6.0, 3.0, 1.0 ]	# salatka warzywna
+[ 
+	[ 1.0, _13, _19, _17, _15 ],	# gyros 5
+	[ 3.0, 1.0, _17, _15, _13 ],	# sushi 4
+	[ 9.0, 7.0, 1.0, 3.0, 5.0 ],	# salatka owocowa 1
+	[ 7.0, 5.0, _13, 1.0, 3.0 ],	# zupa pomidorowa 2
+	[ 5.0, 3.0, _15, _13, 1.0 ],	# losos z piekarnika 3
 ]
 	
 
 calorific = \
 [
-	[ 1.0, 2.0, 8.0, _12, 5.0, 4.0, 6.0, 3.0, 7.0, 9.0 ],	# gyros 
-	[ _12, 1.0, 7.0, _13, 4.0, 3.0, 5.0, 2.0, 6.0, 8.0 ],	# sushi
-	[ _18, _17, 1.0, _19, _14, _15, _13, _16, _12, 2.0 ],	# salatka owocowa
-	[ 2.0, 3.0, 9.0, 1.0, 6.0, 5.0, 7.0, 4.0, 8.0, 9.0 ],	# pizza
-	[ _15, _14, 4.0, _16, 1.0, _12, 2.0, _13, 3.0, 5.0 ],	# jajecznica
-	[ _14, _13, 5.0, _15, 2.0, 1.0, 3.0, _12, 4.0, 6.0 ],	# tosty
-	[ _16, _15, 3.0, _17, _12, _13, 1.0, _14, 2.0, 4.0 ],	# zupa pomidorowa
-	[ _13, _12, 6.0, _14, 3.0, 2.0, 4.0, 1.0, 5.0, 7.0 ],	# rosol
-	[ _17, _16, 2.0, _18, _13, _14, _12, _15, 1.0, 3.0 ],	# losos z piekarnika
-	[ _19, _18, _12, _19, _15, _16, _14, _17, _13, 1.0 ]	# salatka warzywna
+	[ 1.0, _13, 7.0, 5.0, 3.0 ],	# gyros 2
+	[ 3.0, 1.0, 9.0, 7.0, 5.0 ],	# sushi 1
+	[ _17, _19, 1.0, _13, _15 ],	# salatka owocowa 5
+	[ _15, _17, 3.0, 1.0, _13 ],	# zupa pomidorowa 4
+	[ _13, _15, 5.0, 3.0, 1.0 ],	# losos z piekarnika 3
 ]
 
 
 simplicity = \
 [
-	[ 1.0, 3.0, _17, 2.0, _15, _14, _12, _13, _16, _18 ],	# gyros 
-	[ _13, 1.0, _19, _12, _17, _16, _14, _15, _18, _19 ],	# sushi
-	[ 7.0, 9.0, 1.0, 8.0, 3.0, 4.0, 6.0, 5.0, 2.0, _12 ],	# salatka owocowa
-	[ _12, 2.0, _18, 1.0, _16, _15, _13, _14, _17, _19 ],	# pizza
-	[ 5.0, 7.0, _13, 6.0, 1.0, 2.0, 4.0, 3.0, _12, _14 ],	# jajecznica
-	[ 4.0, 6.0, _14, 5.0, _12, 1.0, 3.0, 2.0, _13, _15 ],	# tosty
-	[ 2.0, 4.0, _16, 3.0, _14, _13, 1.0, _12, _15, _17 ],	# zupa pomidorowa
-	[ 3.0, 5.0, _15, 4.0, _13, _12, 2.0, 1.0, _14, _16 ],	# rosol
-	[ 6.0, 8.0, _12, 7.0, 2.0, 3.0, 5.0, 4.0, 1.0, _13 ],	# losos z piekarnika
-	[ 8.0, 9.0, 2.0, 9.0, 4.0, 5.0, 7.0, 6.0, 3.0, 1.0 ]	# salatka warzywna
+	[ 1.0, 3.0, _17, _13, _15 ],	# gyros 4
+	[ _13, 1.0, _19, _15, _17 ],	# sushi 5
+	[ 7.0, 9.0, 1.0, 5.0, 3.0 ],	# salatka owocowa 1
+	[ 3.0, 5.0, _15, 1.0, _13 ],	# zupa pomidorowa 3
+	[ 5.0, 7.0, _13, 3.0, 1.0 ],	# losos z piekarnika 2
 ]
 		   
 	
 def main( ): 
-	"""lamPrice = lambdaMax( price ); 
+	lamPrice = lambdaMax( price ); 
 	print "Lambda Price\t ", lamPrice
 	print "Consists Price\t ", \
 		countMatrixConsistency( price, RI ), \
@@ -136,7 +99,7 @@ def main( ):
 	print "Lambda Time\t ", lamTime
 	print "Consists Time\t ", \
 		countMatrixConsistency( time, RI ), \
-		isMatrixConsistence( time, RI ) """
+		isMatrixConsistence( time, RI )
 		
 	lamDige = lambdaMax( digestibility );
 	print "Lambda Digestibility\t ", lamDige
@@ -144,7 +107,7 @@ def main( ):
 		countMatrixConsistency( digestibility, RI ), \
 		isMatrixConsistence( digestibility, RI ) 
 
-	"""lamCalo = lambdaMax( calorific );
+	lamCalo = lambdaMax( calorific );
 	print "Lambda Calorific\t ", lamCalo
 	print "Consists Calorific\t ", \
 		countMatrixConsistency( calorific, RI ), \
@@ -154,7 +117,7 @@ def main( ):
 	print "Lambda Simplicity\t ", lamSimp
 	print "Consists Simplicity\t ", \
 		countMatrixConsistency( simplicity, RI ), \
-		isMatrixConsistence( simplicity, RI )  """
+		isMatrixConsistence( simplicity, RI ) 
 	
 def normalizeVertically( matrix ):
 	"""Normalizes the given matrix vertically"""
